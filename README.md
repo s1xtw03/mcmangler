42a7b548de14e67d43b9a0508549d52915463f37
s1xtw03 <44380364+s1xtw03@users.noreply.github.com>
2019-09-21 14:28:13 -0400

    reimplemented the thing in python
    hashcat rule processing actually isn't that great

--- README.md
+++ README.md
@@ -1,29 +1,25 @@
 # the mcmangler
 
-This service generates a list of leetspeak candidates combined with common complex human appendages.
+This service generates a list of leetspeak password candidates combined with common complex human appendages.
 
-So for client Acme, it returns:
-
-acme, Acme, ACME, @cme, @cme2017...
-
-Until all possible combinations have been exhausted.
-
-rules-whole and rules-root are processed with hashcat-utils/combipow to generate all the combinations.
+It prints all possible combinations subsituting: a with @, i with 1, o with 0, s with $, and e with 3.  
 
 This is nice to load potential candidates for bruteforcing external services, ssh/ftp on internals, pretty much anything that doesn't have a failure-lockout. 
 
 ```
-jmcg@biscuit:~/mcmangler$ ./mcmangler.sh Acme                                                                                                  
-acm31                     
-acm3!1                           
-acm31!                            
-Acm31                           
-Acm3!1                           
-Acm31!                           
-ACM31                            
-ACM3!1                           
-ACM31!
-{...}
+jmcg@seles:~/util/s1xtw03/mcmangler$ python mcmangler.py -w Microsoft | sort -R | head  
+M1CR0S0FT2018!
+m1cr0s0ft123!
+M1CROSOFT!123
+Micros0ft!123
+Micr0$0ft!123
+M1cr0soft123
+MICRO$0FT!123
+micr0soft!123
+m1cro$oft!1
+micr0soft2019!
+jmcg@seles:~/util/s1xtw03/mcmangler$ python mcmangler.py -w Microsoft | wc -l          
+     585
 ```
 
-Requires [hashcat](https://github.com/hashcat/hashcat) and [hashcat-utils](https://github.com/hashcat/hashcat-utils). 
+The old version used old-w-hashcat/rules-whole and old-w-hashcat/rules-root, processed with hashcat-utils/combipow to generate all the combinations. It also required [hashcat](https://github.com/hashcat/hashcat) and [hashcat-utils](https://github.com/hashcat/hashcat-utils), but those components are reimplemented in python now!!!!!
---
+++ mcmangler.py
@@ -0,0 +1,126 @@
+import os
+import sys
+import argparse
+import itertools
+
+suffixes = ["2018", "2018!", "2019", "2019!", "1", "!1", "1!", "12", "12!", "123", "123!", "!123"]
+
+def main():
+  args = input_handler()
+
+  if args.word:
+    mcmangle(args.word.lower())
+    sys.exit(0) 
+
+  if args.file:
+    try:
+      f = open(args.file)
+    except:
+      print("couldn't read file")
+
+    for line in f:
+        mcmangle(lower(line))
+  else:
+    print("hi")
+
+
+def mcmangle(word):
+  rules_root = build_rules_root(word)
+  rules_combination = build_rules_combination(rules_root)
+
+  for rule_set in rules_combination:
+    transformed = run_rule_set(word, rule_set)
+
+    print(transformed)
+    print(transformed.upper())
+    if transformed[0].isalpha():
+      print(transformed.capitalize())
+
+    for suffix in suffixes:
+      print(transformed + suffix)
+      print(transformed.upper() + suffix)
+      if transformed[0].isalpha():
+        print(transformed.capitalize() + suffix)
+
+
+def run_rule_set(word, rule_set):
+  transformed = word
+
+  for rule in rule_set:
+    occurence = int(rule[1])
+    char_to_replace = rule[2]
+    char_to_insert = rule[9] 
+
+    index_in_word = find_nth(word, char_to_replace, occurence)
+    transformed = transformed[:index_in_word] + char_to_insert + transformed[index_in_word+1:]
+
+  return transformed
+
+
+def build_rules_combination(rules_root):
+  rules_root_combination = []
+
+  for i in range(1, len(rules_root)+1):
+    rules_root_combination = rules_root_combination + list(itertools.combinations(rules_root, i))
+
+  return rules_root_combination
+
+
+def build_rules_root(word):
+  rules_root = []
+
+  a_ct = 0
+  i_ct = 0
+  s_ct = 0
+  e_ct = 0
+  o_ct = 0
+
+  for letter in list(word):
+    if letter == 'a':
+      a_ct = a_ct + 1
+      rules_root.append("%" + str(a_ct) + letter + " Dp ip@")
+    elif letter == 'i':
+      i_ct = i_ct + 1
+      rules_root.append("%" + str(i_ct) + letter + " Dp ip1")
+    elif letter == 's':
+      s_ct = s_ct + 1
+      rules_root.append("%" + str(s_ct) + letter + " Dp ip$")
+    elif letter == 'e':
+      e_ct = e_ct + 1
+      rules_root.append("%" + str(e_ct) + letter + " Dp ip3")
+    elif letter == 'o':
+      o_ct = o_ct + 1
+      rules_root.append("%" + str(o_ct) + letter + " Dp ip0")
+
+  return rules_root
+
+
+#https://stackoverflow.com/questions/1883980/find-the-nth-occurrence-of-substring-in-a-string
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n = n - 1
+    return start
+
+
+def input_handler():
+  desc = """Generate l33tspeak combinations for password brute forcing.
+
+            Quick examples:
+            ./mcmangler.py -w Microsoft 
+            ./mcmangler.py -f words.txt"""
+
+  p = argparse.ArgumentParser(description=desc, formatter_class=argparse.RawTextHelpFormatter)
+  p.add_argument('--word', 
+                  '-w', 
+                  help='a word to mangle')
+  p.add_argument('--file', 
+                  '-f',
+                  help='newline separated file with a bunch of words to mangle')
+
+  args = p.parse_args()
+  return args
+
+
+main()
